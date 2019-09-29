db7d5ec84b700178fe36077525e0c6e97668df91
allison cortez <allizetorc@gmail.com>
2019-09-29 02:58:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,6 +31,7 @@ def update_counting_hash(hash, key)
 else
  hash[key] = 1
 end
-update_counting_hash = { hash => key }
+#update_counting_hash = { hash => key }
+update_counting_hash(hash, key)
 end
 end
